6511964fb2e4bc4380bb4fc2aad60c626bef0ecd
cvieirasp <cvieirasp@gmail.com>
2025-02-12 17:31:40 -0300

    add redux toolkit and configure it

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,11 +1,26 @@
 import Link from "next/link";
-import { Search, Settings } from "lucide-react";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black dark:px-4 dark:py-3">
       {/* Barra de Pesquisa */}
       <div className="flex items-center gap-8">
+        {isSidebarCollapsed && (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 cursor-pointer dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
@@ -18,9 +33,27 @@ const Navbar = () => {
 
       {/* Ícones */}
       <div className="flex items-center">
+        <button
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+        >
+          {isDarkMode ? (
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}
+        </button>
         <Link
           href="/settings"
-          className="hover::bg-gray-100 h-min w-min rounded p-2"
+          className={
+            isDarkMode
+              ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
+          }
         >
           <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
         </Link>
--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,24 +1,68 @@
 "use client";
 
-import { LockIcon } from "lucide-react";
-import Image from "next/image";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import {
+  AlertCircle,
+  AlertOctagon,
+  AlertTriangle,
+  Briefcase,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Layers3,
+  LockIcon,
+  LucideIcon,
+  Search,
+  Settings,
+  ShieldAlert,
+  User,
+  Users,
+  X,
+} from "lucide-react";
+import Image from "next/image";
+import { motion } from "motion/react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 
 const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
-  const sidebarClassNames =
-    "fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64";
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
 
   return (
-    <div className={sidebarClassNames}>
+    <motion.div
+      initial={{ opacity: 0, height: 0 }}
+      animate={{
+        opacity: isSidebarCollapsed ? 0 : 1,
+        height: isSidebarCollapsed ? 0 : "100%",
+      }}
+      transition={{ duration: 0.3 }}
+      className={sidebarClassNames}
+    >
       <div className="flex h-[100%] w-full flex-col justify-start">
         {/* Logo */}
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">
             TASKHIVE
           </div>
+          {!isSidebarCollapsed && (
+            <button
+              className="py-3"
+              onClick={() =>
+                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+              }
+            >
+              <X className="h-6 w-6 cursor-pointer text-gray-800 hover:text-gray-500 dark:text-white" />
+            </button>
+          )}
         </div>
 
         {/* Equipe */}
@@ -36,8 +80,109 @@ const Sidebar = () => {
         </div>
 
         {/* Links */}
+        <nav className="z-10 w-full">
+          <SidebarLink href="/" Icon={Home} label="Home" />
+          <SidebarLink
+            href="/timeline"
+            Icon={Briefcase}
+            label="Linha do Tempo"
+          />
+          <SidebarLink href="/search" Icon={Search} label="Pesquisa" />
+          <SidebarLink href="/settings" Icon={Settings} label="Configurações" />
+          <SidebarLink href="/users" Icon={User} label="Usuários" />
+          <SidebarLink href="/teams" Icon={Users} label="Times" />
+
+          {/* Projetos */}
+          <button
+            className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+            onClick={() => setShowProjects((prev) => !prev)}
+          >
+            <span>Projetos</span>
+            {showProjects ? (
+              <ChevronUp className="h-5 w-5" />
+            ) : (
+              <ChevronDown className="h-5 w-5" />
+            )}
+          </button>
+
+          {/* Prioridades */}
+          <button
+            className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+            onClick={() => setShowPriority((prev) => !prev)}
+          >
+            <span>Prioridades</span>
+            {showPriority ? (
+              <ChevronUp className="h-5 w-5" />
+            ) : (
+              <ChevronDown className="h-5 w-5" />
+            )}
+          </button>
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{
+              opacity: showPriority ? 1 : 0,
+              height: showPriority ? "auto" : 0,
+            }}
+            transition={{ duration: 0.4 }}
+          >
+            <SidebarLink
+              href="/priority/urgent"
+              Icon={AlertCircle}
+              label="Urgente"
+            />
+            <SidebarLink
+              href="/priority/high"
+              Icon={ShieldAlert}
+              label="Alta"
+            />
+            <SidebarLink
+              href="/priority/medium"
+              Icon={AlertTriangle}
+              label="Média"
+            />
+            <SidebarLink
+              href="/priority/low"
+              Icon={AlertOctagon}
+              label="Baixa"
+            />
+            <SidebarLink
+              href="/priority/backlog"
+              Icon={Layers3}
+              label="Backlog"
+            />
+          </motion.div>
+        </nav>
       </div>
-    </div>
+    </motion.div>
+  );
+};
+
+interface SidebarLinkProps {
+  href: string;
+  Icon: LucideIcon;
+  label: string;
+}
+
+const SidebarLink = ({ href, Icon, label }: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`}
+      >
+        {isActive && (
+          <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
+        )}
+
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
 
